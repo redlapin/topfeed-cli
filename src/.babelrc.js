a8c6abc75081855b5f3b0f6e1af8830a76521852
hardfist <1562502418@qq.com>
2018-08-30 11:53:12 +0800

    scss support

--- src/.babelrc.js
+++
@@ -1,4 +0,0 @@
-module.exports = api => {
-	console.error("env:", api.env());
-	return {};
-};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
+import "./a.scss";
+import "./b.less";
+import "./c.css";
 console.log("hello world this is good");
